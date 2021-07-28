b9bb5dbc68bf04287e476488b8d9fc7cfdd2f282
PenguinL <diana1997525@gmail.com>
2021-07-29 00:49:10 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #git clone -o PersonOfGameLibrary https://github.com/Kovalsky95/PersonOfGameLibrary.git
 #git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=3600"
+git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
 git pull --all

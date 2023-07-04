e09d22e8c41409c2108559d5d3c8ca3d755306dc
PenguinLW <diana1997525@gmail.com>
2023-07-04 18:48:31 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,11 +1,14 @@
 #in venv
 #pip install -r req.txt
 #pip freeze > req.txt
+
+git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/PersonOfGameLibrary
 #git clone -o PersonOfGameLibrary https://github.com/PenguinLW/PersonOfGameLibrary.git
 #git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
+
 git pull --all
 git add .
 git commit -m PenguinL

8430c57cfc57587a5e99d3c1cb72b0d0c5d39f4b
PenguinLW <diana1997525@gmail.com>
2023-07-04 19:09:33 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,9 +1,13 @@
 #in venv
+#source ~/venv/bin/activate
 #pip install -r req.txt
 #pip freeze > req.txt
+chmod u+x *.sh *.py
 
 git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/PersonOfGameLibrary
 #git clone -o PersonOfGameLibrary https://github.com/PenguinLW/PersonOfGameLibrary.git
+#git config --global user.email "diana1997525@gmail.com"
+#git config --global user.name "PenguinLW"
 #git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
@@ -13,3 +17,4 @@ git pull --all
 git add .
 git commit -m PenguinL
 git push --set-upstream PersonOfGameLibrary master
+#deactivate
